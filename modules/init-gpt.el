f4b9346c904f0791ebd087f8b87b6ea32bdab902
Janne Sauvala <janne.sauvala@gmail.com>
2024-07-01 12:02:11 +0300

    Add new backends for gptel

--- modules/init-gpt.el
+++ modules/init-gpt.el
@@ -1,8 +1,25 @@
 ;;;; init-gpt.el -*- lexical-binding: t; no-byte-compile: t; -*-
 
 (use-package gptel
+  :custom
+  (gptel-default-mode 'org-mode)
   :config
-  (setq gptel-default-mode 'org-mode)
+  (gptel-make-azure
+    "UAI"
+    :protocol "https"
+    :host "openai-janne-sauvala.openai.azure.com"
+    :endpoint
+    "/openai/deployments/gpt-4-1106-preview/chat/completions?api-version=2024-02-01"
+    :models '("gpt-4-1106-preview")
+    :stream t)
+  (gptel-make-azure
+    "UAIO"
+    :protocol "https"
+    :host "openai-janne-sauvala-eus.openai.azure.com"
+    :endpoint
+    "/openai/deployments/got4o/chat/completions?api-version=2024-02-01"
+    :models '("got4o")
+    :stream t)
   (add-hook 'gptel-post-stream-hook 'gptel-auto-scroll)
   (add-hook 'gptel-post-response-functions 'gptel-end-of-response))
 

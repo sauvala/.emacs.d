e6952e53dddde781f755d28440706ec301c7dbd0
Janne Sauvala <janne.sauvala@gmail.com>
2024-10-24 09:09:38 +0300

    Tune Tramp configs

--- modules/init-tramp.el
+++ modules/init-tramp.el
@@ -4,10 +4,10 @@
   :ensure nil
   :custom
   (tramp-copy-size-limit (* 10240 2))
-  (tramp-use-connection-share t)
+  (tramp-use-connection-share nil)
   :config
-  (setq tramp-default-method "rsync")
-  ;;(setq tramp-default-method "ssh")
+  ;;(setq tramp-default-method "rsync")
+  (setq tramp-default-method "ssh")
   (setq remote-file-name-inhibit-locks t)
   (setq remote-file-name-inhibit-cache 180)
   (setq tramp-completion-reread-directory-timeout 180)
@@ -21,7 +21,7 @@
   (setq tramp-verbose 1)
   (add-to-list 'tramp-connection-properties
                (list (regexp-quote "/ssh:")
-                     "direct-async-process" t))
+                     "tramp-direct-async-process" t))
   (setq tramp-chunksize 500)
   ;;(setq tramp-chunksize nil)
   (setq tramp-inline-compress-start-size 1000000) ;; Enable compression above 1MB
@@ -30,6 +30,15 @@
     (if (file-remote-p (buffer-file-name))
         (setq-local vc-handled-backends nil)))
   ;;(add-hook 'find-file-hook 'js-vc-off-if-remote)
+
+  (connection-local-set-profile-variables
+   'remote-direct-async-process
+   '((tramp-direct-async-process . t)))
+
+
+  (connection-local-set-profiles
+   '(:application tramp :machine "jannesauvala-ubuntu-dev-vm")
+   'remote-direct-async-process)
   )
 
 (use-package async
